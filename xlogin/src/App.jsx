370c3ac4d2993c8617129ca8c931d26cf8dd1ab1
Aradhika Ramesh <rugved.bmw@gmail.com>
2024-02-28 07:35:47 +0530

    Final-Commit

---
+++ xlogin/src/App.jsx
@@ -0,0 +1,63 @@
+
+import "./App.css";
+import { useState } from "react";
+
+export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === "user" && password === "password") {
+      setError("");
+      setIsSubmitted(true);
+    } else {
+      setError("Invalid username or password");
+      setIsSubmitted(false);
+    }
+  };
+  return (
+    <div>
+      {/* <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2> */}
+      <h1> Login Page</h1>
+      {isSubmitted ? (
+        <div>
+          <p> Welcome, {username}</p>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          {error && <p>{error}</p>}
+
+          <div>
+            <label htmlFor="username">UserName:</label>
+            <input
+              onChange={(e) => setUsername(e.target.value)}
+              type="text"
+              value={username}
+              placeholder="username"
+              id="username"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              value={password}
+              placeholder="password"
+              id="password"
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
